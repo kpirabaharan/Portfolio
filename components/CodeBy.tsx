26fd8ab2c971b1e777d97b8e87ebf826380cfd6c
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-09-17 13:37:58 -0400

    Updated Code By to Animated With Framer
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/CodeBy.tsx
+++ components/CodeBy.tsx
@@ -1,22 +1,50 @@
 'use client';
 
+import { useState } from 'react';
+
+import { motion } from 'framer-motion';
+
 const CodeBy = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const animate = isHovered ? 'open' : 'closed';
+
+  const transition = {
+    duration: 0.5,
+    delay: 0.1,
+  };
+
   return (
-    <div className='flex gap-x-1 group cursor-pointer'>
+    <div
+      className='flex gap-x-1 group cursor-pointer'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <p className='font-light group-hover:rotate-full transition duration-700'>
         &copy;{' '}
       </p>
-      <div className='overflow-hidden text-clip whitespace-nowrap relative'>
-        <p className='group-hover:-translate-x-[69px] transition duration-700'>
+      <motion.div
+        initial='closed'
+        animate={animate}
+        variants={{
+          open: { width: '200px' },
+          closed: { width: '119px' },
+        }}
+        transition={transition}
+        className='overflow-hidden text-clip whitespace-nowrap relative'
+      >
+        <motion.p
+          initial='closed'
+          animate={animate}
+          variants={{
+            open: { x: -70 },
+            closed: { x: 0 },
+          }}
+          transition={transition}
+        >
           &nbsp;Code by Keeshigan Pirabaharan
-          {/* <span className='opacity-0 group-hover:opacity-100'> */}
-          {/* </span> */}
-        </p>
-        <div
-          className='absolute bg-[#004c71] w-[200px] h-full top-0 left-[119px]
-          group-hover:translate-x-[65px] transition duration-700'
-        />
-      </div>
+        </motion.p>
+      </motion.div>
     </div>
   );
 };
