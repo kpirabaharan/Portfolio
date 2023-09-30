a3b1e7cbe5f50acc2454ce62d22fee0c6fcc163e
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-09-30 02:01:03 -0400

    Magnetic Contact Buttons
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import {
   MailIcon,
   PhoneIcon,
@@ -16,6 +16,20 @@ import SplashOut from '@/app/components/SplashOut';
 import { StandingNavbar } from '@/app/components/navbar/StandingNavbar';
 import { styles } from '@/lib/styles';
 import { Separator } from '@/components/ui/separator';
+import MagneticComponent from '@/hoc/MagneticComponent';
+
+interface ContactLinkProps extends PropsWithChildren {}
+
+const ContactLink = ({ children }: ContactLinkProps) => {
+  return (
+    <MagneticComponent
+      className='flex gap-x-4 items-center w-fit group'
+      modifier={{ x: 0.25, y: 0.5 }}
+    >
+      {children}
+    </MagneticComponent>
+  );
+};
 
 const ContactPage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,57 +47,63 @@ const ContactPage = () => {
           </h1>
           <Separator className='my-6 h-[2px]' />
           <div className='grid grid-cols-1 md:grid-cols-2 gap-y-8 '>
-            <div className='flex gap-x-4 items-center'>
+            <ContactLink>
               <MailIcon size={24} />
-              <a href='mailto:kpirabaharan3@gmail.com' className='text-xl'>
+              <a
+                href='mailto:kpirabaharan3@gmail.com'
+                className='text-xl group-hover:underline underline-offset-8'
+              >
                 kpirabaharan3@gmail.com
               </a>
-            </div>
+            </ContactLink>
 
-            <div className='flex gap-x-4 items-center '>
+            <ContactLink>
               <PhoneIcon size={24} />
-              <a href='tel:4166173498' className='text-xl'>
+              <a
+                href='tel:4166173498'
+                className='text-xl group-hover:underline underline-offset-8'
+              >
                 (416)-617-3498
               </a>
-            </div>
+            </ContactLink>
 
-            <div className='flex gap-x-4 items-center '>
+            <ContactLink>
               <LinkedinIcon size={24} />
               <a
                 href='https://www.linkedin.com/in/kpirabaharan/'
-                className='text-xl'
+                className='text-xl group-hover:underline underline-offset-8'
                 target='_blank'
               >
                 kpirabaharan
               </a>
-            </div>
+            </ContactLink>
 
-            <div className='flex gap-x-4 items-center '>
+            <ContactLink>
               <GithubIcon size={24} />
               <a
                 href='https://github.com/kpirabaharan'
-                className='text-xl'
+                className='text-xl group-hover:underline underline-offset-8'
                 target='_blank'
               >
                 kpirabaharan
               </a>
-            </div>
+            </ContactLink>
 
-            <div className='flex gap-x-4 items-center '>
+            <ContactLink>
               <FileTextIcon size={24} />
               <a
                 href='./Keeshigan-Pirabaharan-Resume.pdf'
-                className='text-xl'
+                className='text-xl group-hover:underline underline-offset-8'
                 target='_blank'
               >
                 Resume
               </a>
-            </div>
+            </ContactLink>
 
-            <div className='flex gap-x-4 items-center '>
+            <ContactLink>
               <MapPinIcon size={24} />
               <p className='text-xl'>Toronto, ON</p>
-            </div>
+            </ContactLink>
           </div>
         </div>
       </div>
