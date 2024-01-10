91fafff255b355615de915c74fcc8194521687c6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-09 20:45:38 -0500

    Updated Last Modified to Track production Branch
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- actions/getLastCommitDate.ts
+++ actions/getLastCommitDate.ts
@@ -9,7 +9,7 @@ export const getLastCommitDate = async () => {
   const { data } = await octokit.rest.repos.getBranch({
     owner: 'kpirabaharan',
     repo: 'next-portfolio',
-    branch: 'master',
+    branch: 'production',
   });
 
   const date = data.commit.commit.committer?.date ?? '2023-09-26T05:05:54Z';
