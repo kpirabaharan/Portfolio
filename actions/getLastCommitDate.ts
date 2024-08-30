bad88142a6323ee527a1d00f55301cd8e9b2420f
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-08-30 00:52:27 -0400

    Track Master Branch
    Changed Production Branch from production to master
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- actions/getLastCommitDate.ts
+++ actions/getLastCommitDate.ts
@@ -4,7 +4,7 @@ export const getLastCommitDate = async () => {
   const { data } = await octokit.rest.repos.getBranch({
     owner: 'kpirabaharan',
     repo: 'next-portfolio',
-    branch: 'production',
+    branch: 'master',
   });
 
   const date = data.commit.commit.committer?.date ?? '2024-02-23T05:05:54Z';
